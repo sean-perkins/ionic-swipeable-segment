93dc93727823071556348bd78c82b0a5a09eadd4
Sean Perkins <sean@ionic.io>
2022-12-12 12:38:43 -0500

    feat: swipeable segment container

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  @ViewChild('swipeContainer', { static: true }) swipeContainer!: ElementRef<HTMLElement>;
+
+  activeSegment = 'first';
+
+  onScroll(ev: any) {
+    const scrollLeft = this.swipeContainer.nativeElement.scrollLeft;
+    const scrollWidth = this.swipeContainer.nativeElement.scrollWidth;
+    const clientWidth = this.swipeContainer.nativeElement.clientWidth;
+
+    if (scrollLeft === 0) {
+      this.activeSegment = 'first';
+    } else if (scrollLeft === clientWidth) {
+      this.activeSegment = 'second';
+    } else if (scrollLeft === scrollWidth - clientWidth) {
+      this.activeSegment = 'third';
+    }
+  }
+
+  onSegmentChange(ev: any) {
+    const value = ev.detail.value;
+    switch (value) {
+      case 'first':
+        this.swipeContainer.nativeElement.scrollTo({
+          left: 0,
+        });
+        break;
+      case 'second':
+        this.swipeContainer.nativeElement.scrollTo({
+          left: this.swipeContainer.nativeElement.scrollWidth / 2,
+        });
+        break;
+      case 'third':
+        this.swipeContainer.nativeElement.scrollTo({
+          left: this.swipeContainer.nativeElement.scrollWidth,
+        });
+        break;
+    }
+  }
 
 }
